a99b4e9d4ae44720229ce461c143583e43d6ea4e
Melany Pelletier <melany.pelletier@gmail.com>
2019-03-15 10:31:45 -0400

    Make javascript overlays work on h3

--- _patterns/cards/overlay-student-dashboard.js
+++ _patterns/cards/overlay-student-dashboard.js
@@ -8,3 +8,8 @@ $leftArrowStudent.on('click', function () {
 $closeLabel.on('click', function () {
   $('.job-description').hide();
 });
+
+var $companyPopup = $('.company-popup');
+$companyPopup.on('click', function () {
+  $(this).parent().siblings('.job-data').children(".job-description").show();
+});
